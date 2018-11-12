34d8df3f835e4e2a5462a9da88998f80d54bfecd
biancacharlotin <biancacharlotin@gmail.com>
2018-11-12 14:48:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,6 @@
 def line(number)
-   number[]
-   if katz_deli.map
-   puts "The line is currently: #{katz_deli}"
-  else
-    puts "The line is currently empty."
-end
+   if number< 1
+puts ""
 end
 
 
